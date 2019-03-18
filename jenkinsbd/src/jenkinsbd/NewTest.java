9060e219c40996e1af087e5ae816a61ebad8f1b1
tushuailun1 <2495852145@qq.com>
2019-03-18 11:34:55 +0800

    baidu

---
+++ jenkinsbd/src/jenkinsbd/NewTest.java
@@ -0,0 +1,9 @@
+package jenkinsbd;
+
+import org.testng.annotations.Test;
+
+public class NewTest {
+  @Test
+  public void f() {
+  }
+}
---
+++ jenkinsbd/src/jenkinsbd/baidu.java
@@ -0,0 +1,27 @@
+package jenkinsbd;
+
+
+	import java.net.MalformedURLException;
+	import org.openqa.selenium.WebDriver;
+	import org.openqa.selenium.chrome.ChromeDriver;
+	import org.testng.annotations.AfterClass;
+	import org.testng.annotations.BeforeClass;
+
+	public class baidu {
+		public static WebDriver driver;
+		public static String url;
+		@BeforeClass
+		public static void open() throws MalformedURLException {
+		System.setProperty("webdriver.chrome.driver", "./tools/chromedriver.exe");
+			driver=new ChromeDriver();
+			
+			url="www.baidu.com";
+			
+		}
+		@AfterClass
+		public void guanbi() {
+			//driver.quit();
+		}
+	}
+
+
